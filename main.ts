de4783c3e8a4c8b05dc105914f96ff4006cf79f4
katherinjme <86328748+katherinjme@users.noreply.github.com>
2021-06-22 17:35:57 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("katherin")
+})
